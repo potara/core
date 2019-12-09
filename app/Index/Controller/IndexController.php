0b0830310b86d738fbc870570bb83421d727784f
Bruno Lima <brunolimame@gmail.com>
2019-12-09 13:45:17 -0300

    Updating Copyright Year

--- app/Index/Controller/IndexController.php
+++ app/Index/Controller/IndexController.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * This file is part of the Potara Framework (https://potara.org)
+ * This file is part of the Potara (https://potara.org)
  *
  * @see       https://github.com/potara/core
- * @copyright Copyright (c) 2018-2019 Bruno Lima
- * @author    Bruno Lima <eu@brunolima.me>
+ * @copyright Copyright (c) 2018-2020 Bruno Lima
+ * @author    Bruno Lima <brunolimame@gmail.com>
  * @license   https://github.com/potara/core/blob/master/LICENSE (MIT License)
  */
 
 namespace App\Index\Controller;
-
 
 use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
--- src/Core/Crud/Entity/ConvertToArraypg.php
+++ src/Core/Crud/Entity/ConvertToArraypg.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the Potara (https://potara.org)
+ *
+ * @see       https://github.com/potara/core
+ * @copyright Copyright (c) 2018-2020 Bruno Lima
+ * @author    Bruno Lima <brunolimame@gmail.com>
+ * @license   https://github.com/potara/core/blob/master/LICENSE (MIT License)
+ */
 
 namespace Potara\Core\Crud\Entity;
 

e8e51fbdd9cf58c61aaa322f748ea2b77931f49c
Bruno Lima <brunolimame@gmail.com>
2020-02-02 18:01:21 -0300

    Command line module started

---
+++ src/Core/Command/teste.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of the Potara (https://potara.org)
+ *
+ * @see       https://github.com/potara/core
+ * @copyright Copyright (c) 2018-2020 Bruno Lima
+ * @author    Bruno Lima <brunolimame@gmail.com>
+ * @license   https://github.com/potara/core/blob/master/LICENSE (MIT License)
+ */
+
+namespace Potara\Core\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class teste extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'potara:create-user';
+
+    protected function configure()
+    {
+        $this
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Creates a new user.')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This command allows you to create a user...');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // outputs multiple lines to the console (adding "\n" at the end of each line)
+        $output->writeln([
+            'User Creator',
+            '============',
+            '',
+        ]);
+
+        // outputs a message followed by a "\n"
+        $output->writeln('Whoa!');
+
+        // outputs a message without adding a "\n" at the end of the line
+        $output->write('You are about to ');
+        $output->write('create a user.');
+
+        return 0;
+    }
+}

e2014fbbce955fd675703dfb948e852c62e96389
Bruno Lima <brunolimame@gmail.com>
2021-06-28 21:32:44 -0300

    atualização do ano

--- src/Core/Crud/Entity/ConvertToSerialize.php
+++ src/Core/Crud/Entity/ConvertToSerialize.php
@@ -3,7 +3,7 @@
  * This file is part of the Potara (https://potara.org)
  *
  * @see       https://github.com/potara/core
- * @copyright Copyright (c) 2018-2020 Bruno Lima
+ * @copyright Copyright (c) 2018-2021 Bruno Lima
  * @author    Bruno Lima <brunolimame@gmail.com>
  * @license   https://github.com/potara/core/blob/master/LICENSE (MIT License)
  */
